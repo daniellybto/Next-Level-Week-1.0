a5635ee1526d68d5f9e083557b5ee0d2e642dd44
Dany.Win10 <danielly.eng@hotmail.com>
2020-06-17 19:43:20 -0300

    Projeto Finalizado!

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const server = express();
 
+//pegar o bancod e dados
+const db = require("./database/db");
+
 //configurar pasta pública - ou seja arquivos estáticos/públicos
 server.use(express.static("public"));
+
+//habilitar o uso do req.body na nossa aplicação - DADOS ENVIADOS VIA METHOD: 'POST'
+server.use(express.urlencoded({ extended: true }))
 
 //utilizando TEMPLATE ENGINE
 const nunjucks = require("nunjucks");
@@ -23,10 +29,77 @@ server.get("/", (req, res) => {
   return res.render("index.html")
 });
 server.get("/create-point", (req, res) => {
+
+  //req.query: Query String da nossa URL
+  console.log(req.query)
+
   return res.render("create-point.html")
 });
+
+server.post("/savepoint", (req, res) => {
+  //req.body: O Corpo do nosso formulário
+  // console.log(req.body)
+
+  //inserir dados no Banco de Dados
+
+
+  // 2 - inserir dados na tabela
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) 
+    VALUES (?,?,?,?,?,?,?);`
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err){
+    if(err){
+      return console.log("Algo deu errado >>> " + err)
+    }
+    // console.log(this)
+    return res.render("create-point.html", {saved: true})
+
+  }
+  
+  db.run(query, values, afterInsertData)
+})
+
 server.get("/search", (req, res) => {
-  return res.render("search-results.html")
+
+  const search = req.query.search
+
+  if(search == ''){
+    //pesquisa vazia
+    return res.render("search-results.html", {total: 0})
+  }
+
+  //pegar dados do banco de dados:
+  // 3 - consultar os dados na tabela
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+    if(err){
+      console.log("Algo deu errado >>> " + err)
+      return res.send("Algo deu errado ! ")
+    }
+    const total = rows.length
+
+    //mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", {places: rows, total: total})
+  })
+  
 });
 
 
